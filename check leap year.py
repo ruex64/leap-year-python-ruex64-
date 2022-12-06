c7e2ca7593314b81aed050b90fdc85fa150e3c4e
ruex64/Rudraksh Mahajan <112755580+ruex644@users.noreply.github.com>
2022-12-06 09:48:08 +0530

    new changes added
    A new loop added to check if the loop entered data is correct or not

--- check leap year.py
+++ check leap year.py
@@ -3,20 +3,24 @@ b1= input("enter the ending year in the format dd/mm/yyyy : ")
 year=int(a1[-4:])
 year2=int(b1[-4:])
 s = []                                             
-b = []                                             
-for i in range(year,(year2)+1):                    
-    if i % 4 == 0:                                 
-        if i % 100 != 0:
-            s.append(i)                            
+b = []
+if year2>year:
+    
+    for i in range(year,(year2)+1):                    
+        if i % 4 == 0:                                 
+            if i % 100 != 0:
+                s.append(i)                            
                                                    
-        elif i % 100 == 0:
-            if i % 400 == 0:
-                s.append(i)
+            elif i % 100 == 0:
+                if i % 400 == 0:
+                    s.append(i)
 
-            else:
-                b.append(i)
-    else:
-        b.append(i)
+                else:
+                    b.append(i)
+        else:
+            b.append(i)
 
-print("leap year: ", s)                                           
-print("Non leap year: " , b)
+    print("leap year: ", s)                                           
+    print("Non leap year: " , b)
+else:
+    print('Wrong Dates')
